612e23949965ffd5eb064db623aea12694e0d5fb
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-20 19:24:34 +0200

    Triggering autograder final check

--- Downloads/hw3-romsh2004-main/hw3-romsh2004-main/Algebra.java
+++ Downloads/hw3-romsh2004-main/hw3-romsh2004-main/Algebra.java
@@ -5,22 +5,7 @@
 
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+
 	}  
 
 public static int plus(int x1, int x2) {
@@ -146,6 +131,9 @@ public static int sqrt(int x) {
             high = minus(mid, 1); 
         }
     }
+	
+
+	//עםקןחרקעםןחקעםןקרחעםןקרחעםןקרחעןחק
     
     return ans;
 }	  
